75249df85f4e2a8f890315478fc63484ee2d00cf
Timo Mämecke <timo@maemecke.com>
2015-11-30 22:51:35 +0100

    Support for stateless Components 🎉
    Also removed unnecessary code and added some comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,21 @@
 import React from "react"
 import classNames from "classnames/bind"
 
+// Wrap potential stateless Components and start this whole thingy!
+function receiveComponent(Component, styles) {
+  let _Component = Component
+
+  // Stateless Components are just functions like other React Components, but
+  // they don't have a render method, because they'll render directly
+  if (!("prototype" in Component && typeof Component.prototype.render === "function")) {
+    _Component = wrapStatelessComponent(Component)
+  }
+
+  return transformedElement(_Component, styles)
+}
+
+// return a new Component based on the original Component
+// with a transformed renderer
 var transformedElement = (Component, styles) => {
   return class extends Component {
     render() {
@@ -10,55 +25,85 @@ var transformedElement = (Component, styles) => {
   }
 }
 
+// transform the element
+// - recursive transform its children (see recursiveTransform())
+// - Generate the className
+// - copy the props, inject the new className
+// - return a cloned element
 var transformElement = (element, cx) => {
-  let children = recursiveTransform(element.props.children, cx)
-  let _props
-  if ("prototype" in element && typeof element.prototype.render === "function") {
-    _props = element.props
+  // Straight return elements, which render to undefined
+  if (!element) {
+    return element
   }
+
+  let _props,
+    _className,
+
+    // Here we start the recursion
+    children = recursiveTransform(element.props.children, cx)
+
+  // Support string classnames
+  if (typeof element.props.className === "string") {
+    _className = element.props.className.split(" ")
+  }
+
+  // Support object classnames for classnames module
   else {
-    let _className
-    if (typeof element.props.className === "string") {
-      _className = element.props.className.split(" ")
-    } else {
-      _className = element.props.className
-    }
-
-    _props = {
-      className: cx(_className)
-    }
-    Object.assign(_props, ...element.props)
-
-
-    if (_props.className === "")
-      delete _props.className
+    _className = element.props.className
   }
+
+  // copy props and override the className
+  _props = {
+    ...element.props,
+    className: cx(_className)
+  }
+
+  // delete the className, if it is empty
+  // Note:
+  // ... yeah, this is kinda silly, we could've checked earlier if the className
+  // iss empty. But I think it's less code. Less code makes our files smaller,
+  // and that's a good thing.
+  if (_props.className === "")
+    delete _props.className
 
   return React.cloneElement(element, _props, children)
 }
 
+// handler for recursive looping and delegating children of a
+// Component or simple Element
 function recursiveTransform(element, cx) {
-  if (typeof element === "string")
-    return element
-
+  // Straight transform a valid element
   if (React.isValidElement(element)) {
     return transformElement(element, cx)
   }
 
-  if (Object.prototype.toString.call(element) === "[object Array]") {
+  // Loop through an array of elements
+  else if (Object.prototype.toString.call(element) === "[object Array]") {
     return React.Children.map(element, (child) => {
-      if (React.isValidElement(child)) {
-        return transformElement(child, cx)
-      } else {
-        return child
-      }
+      return recursiveTransform(child, cx)
     })
   }
 
+  // This can either be string or undefined.
+  // We don't need to transform them
+  else {
+    return element
+  }
 }
 
-function transformCssModule(Component, styles) {
-  return transformedElement(Component, styles)
+// With React 0.14 we have these awesome stateless Components, but we need
+// to wrap them in secret to a normal Component.
+function wrapStatelessComponent(Component) {
+  return class extends React.Component {
+    constructor() {
+      super()
+      this.displayName = Component.name
+    }
+
+    render() {
+      return Component(this.props)
+    }
+  }
 }
 
-export default transformCssModule
+export default receiveComponent
